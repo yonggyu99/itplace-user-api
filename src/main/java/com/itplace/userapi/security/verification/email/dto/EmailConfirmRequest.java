74aca2d75f9422793d06974aeffdd2188f016e77
Eddie Lee <devleehy@gmail.com>
2025-07-10 15:45:15 +0900

    feat: Email 인증 코드 확인 요청 DTO

---
+++ src/main/java/com/itplace/userapi/security/verification/email/dto/EmailConfirmRequest.java
@@ -0,0 +1,16 @@
+package com.itplace.userapi.security.verification.email.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailConfirmRequest {
+    @Email(message = "이메일 형식을 맞춰주세요.")
+    private String email;
+
+    @Pattern(regexp = "\\d{6}", message = "인증번호는 6자리 숫자여야 합니다.")
+    private String verificationCode;
+}
