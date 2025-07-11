4e1bb9e607c163f6bfcf342e4b35ae62139794a7
Eddie Lee <devleehy@gmail.com>
2025-07-11 09:31:58 +0900

    feat: UplusData entity 구현
    - 가상의 유플러스 회원 데이터를 담을 entity

---
+++ src/main/java/com/itplace/userapi/user/entity/UplusData.java
@@ -0,0 +1,32 @@
+package com.itplace.userapi.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "uplusData")
+public class UplusData {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name", length = 30)
+    private String name;
+
+    @Column(name = "phoneNumber", length = 11)
+    private String phoneNumber;
+
+    @Column(name = "gender", length = 5)
+    private Gender gender;
+
+    @Column(name = "birthday", length = 8)
+    private LocalDate birthday;
+
+    @Column(name = "membershipId", length = 16)
+    private String membershipId;
+}
