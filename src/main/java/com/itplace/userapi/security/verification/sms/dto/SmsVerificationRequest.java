6b830ab73ebac86b9dfff4339eee87494d7ca717
Eddie Lee <devleehy@gmail.com>
2025-07-11 14:40:46 +0900

    feat: 문자 인증 DTO validation 추가

---
+++ src/main/java/com/itplace/userapi/security/verification/sms/dto/SmsVerificationRequest.java
@@ -0,0 +1,18 @@
+package com.itplace.userapi.security.verification.sms.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SmsVerificationRequest {
+
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+
+    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
+    @Pattern(regexp = "^010[0-9]{8}$", message = "휴대폰 번호는 '010'으로 시작하는 11자리 숫자여야 합니다.")
+    private String phoneNumber;
+}
