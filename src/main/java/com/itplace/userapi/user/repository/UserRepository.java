5bbb239c58b485f14fb1de9b765a75796b31f1f5
Eddie Lee <devleehy@gmail.com>
2025-07-11 15:47:48 +0900

    feat: UserRepository 기능 추가
    - 이메일이나 휴대폰 번호 중 하나라도 가입된 것이 있다면 가입이 불가능 하기 때문에 or 조건으로 검색되는 메서드 추가

--- src/main/java/com/itplace/userapi/user/repository/UserRepository.java
+++ src/main/java/com/itplace/userapi/user/repository/UserRepository.java
@@ -8,4 +8,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
+    Optional<User> findByEmailOrPhoneNumber(String email, String phoneNumber);
 }
