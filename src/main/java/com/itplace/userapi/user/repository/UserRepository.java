096a66bdc35e421df0579103a02f04c768a4419a
Eddie Lee <devleehy@gmail.com>
2025-07-11 09:44:26 +0900

    feat: UserRepository 구현

---
+++ src/main/java/com/itplace/userapi/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.itplace.userapi.user.repository;
+
+import com.itplace.userapi.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumber(String phoneNumber);
+}
