fe8dc31b7176db24cefca9f03e2ebd6bd94516f9
Eddie Lee <devleehy@gmail.com>
2025-07-12 02:38:49 +0900

    feat: SignUpRequest 구현

---
+++ src/main/java/com/itplace/userapi/security/auth/dto/request/SignUpRequest.java
@@ -0,0 +1,47 @@
+package com.itplace.userapi.security.auth.dto.request;
+
+import com.itplace.userapi.user.entity.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class SignUpRequest {
+
+    @NotBlank(message = "이름은 필수 항목입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "전화번호는 필수 항목입니다.")
+    @Pattern(regexp = "^010[0-9]{8}$", message = "전화번호 형식이 올바르지 않습니다.")
+    private String phoneNumber;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Size(min = 6, max = 30, message = "비밀번호는 6자 이상 30자 이하로 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수 항목입니다.")
+    private String passwordConfirm;
+
+    @NotBlank(message = "성별은 필수 항목입니다.")
+    private Gender gender;
+
+    @NotBlank(message = "멤버십 번호는 필수 항목입니다.")
+    private String membershipId;
+
+    @NotBlank(message = "생년월일은 필수 항목입니다.")
+    private LocalDate birthday;
+
+    @NotBlank(message = "등록 ID는 필수 항목입니다.")
+    private String registrationId;
+}
