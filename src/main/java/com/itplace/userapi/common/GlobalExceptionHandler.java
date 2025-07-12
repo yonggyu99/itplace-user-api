734e2f7f510cbe878715ec346e60671e9de16273
Eddie Lee <devleehy@gmail.com>
2025-07-12 15:11:04 +0900

    feat: GlobalExceptionHandler 구현
    - 테스트용으로 추후 예외 정리 예정

--- src/main/java/com/itplace/userapi/common/GlobalExceptionHandler.java
+++ src/main/java/com/itplace/userapi/common/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.itplace.userapi.common;
 
+import com.itplace.userapi.common.exception.EmailVerificationException;
+import com.itplace.userapi.common.exception.SmsVerificationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ApiResponse<?> handleIllegalStateException(IllegalStateException ex) {
-        return ApiResponse.of(BaseCode.TEST, null);
+        return ApiResponse.of(BaseCode.Test, null);
+    }
+
+    @ExceptionHandler(SmsVerificationException.class)
+    public ApiResponse<?> handleSmsVerificationException(SmsVerificationException ex) {
+        return ApiResponse.of(ex.getCode(), null);
+    }
+
+    @ExceptionHandler(EmailVerificationException.class)
+    public ApiResponse<?> handleEmailVerificationException(EmailVerificationException ex) {
+        return ApiResponse.of(ex.getCode(), null);
     }
 }
