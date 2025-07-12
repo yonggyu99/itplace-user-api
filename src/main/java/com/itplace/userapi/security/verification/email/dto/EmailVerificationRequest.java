4eb8584ea3431aa8a28b7137ae04a516895f62af
Eddie Lee <devleehy@gmail.com>
2025-07-12 15:11:42 +0900

    feat: registrationId 추가

--- src/main/java/com/itplace/userapi/security/verification/email/dto/EmailVerificationRequest.java
+++ src/main/java/com/itplace/userapi/security/verification/email/dto/EmailVerificationRequest.java
@@ -1,6 +1,7 @@
 package com.itplace.userapi.security.verification.email.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,9 @@ import lombok.Setter;
 @Setter
 public class EmailVerificationRequest {
 
+    @NotBlank(message = "등록 ID를 입력해주세요.")
+    private String registrationId;
+
     @Email(message = "이메일 형식을 맞춰주세요")
-    String email;
+    private String email;
 }
