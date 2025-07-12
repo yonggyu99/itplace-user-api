fea74d5df823ca7fc14267f2342eb9170ebd5ad3
Eddie Lee <devleehy@gmail.com>
2025-07-12 15:12:37 +0900

    feat: Email 컨트롤러 개선

--- src/main/java/com/itplace/userapi/security/verification/email/controller/EmailController.java
+++ src/main/java/com/itplace/userapi/security/verification/email/controller/EmailController.java
@@ -1,11 +1,11 @@
 package com.itplace.userapi.security.verification.email.controller;
 
 import com.itplace.userapi.common.ApiResponse;
+import com.itplace.userapi.common.BaseCode;
 import com.itplace.userapi.security.verification.email.dto.EmailConfirmRequest;
 import com.itplace.userapi.security.verification.email.dto.EmailVerificationRequest;
 import com.itplace.userapi.security.verification.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +20,14 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/email")
-    public ResponseEntity<ApiResponse<Void>> send(@RequestBody @Validated EmailVerificationRequest request) {
+    public ApiResponse<Void> send(@RequestBody @Validated EmailVerificationRequest request) {
         emailService.send(request);
-        return null;
+        return ApiResponse.ok(BaseCode.EMAIL_SEND_SUCCESS);
     }
 
     @PostMapping("/email/confirm")
-    public ResponseEntity<ApiResponse<Void>> confirm(@RequestBody @Validated EmailConfirmRequest request) {
+    public ApiResponse<Void> confirm(@RequestBody @Validated EmailConfirmRequest request) {
         emailService.confirm(request);
-        return null;
+        return ApiResponse.ok(BaseCode.EMAIL_VERIFICATION_SUCCESS);
     }
 }
