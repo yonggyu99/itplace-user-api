1a02f243704234b529487b4c3fa52f3db18dc00f
Eddie Lee <devleehy@gmail.com>
2025-07-12 15:16:35 +0900

    feat: CustomUserDetails 구현
    - Spring Security 기반 자체 로그인용 UserDetails

---
+++ src/main/java/com/itplace/userapi/security/auth/dto/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package com.itplace.userapi.security.auth.dto;
+
+import com.itplace.userapi.user.entity.User;
+import java.util.Collection;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+}
