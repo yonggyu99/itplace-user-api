9e43891c5dea97b6297286f93560d4c6ce4a92a2
Eddie Lee <devleehy@gmail.com>
2025-07-16 16:45:09 +0900

    feat: WebConfig 구현

---
+++ src/main/java/com/itplace/userapi/common/WebConfig.java
@@ -0,0 +1,17 @@
+package com.itplace.userapi.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해
+                .allowedOrigins("http://localhost:3000") // 허용할 Origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 허용 여부
+    }
+}
