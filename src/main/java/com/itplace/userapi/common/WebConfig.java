3f16d2296a80a550ef9b876a5defadee45802aa4
Eddie Lee <devleehy@gmail.com>
2025-07-16 21:14:55 +0900

    feat: WebConfig 수정

--- src/main/java/com/itplace/userapi/common/WebConfig.java
+++ src/main/java/com/itplace/userapi/common/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해
-                .allowedOrigins("http://localhost:3000") // 허용할 Origin
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174") // 허용할 Origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 허용 여부
