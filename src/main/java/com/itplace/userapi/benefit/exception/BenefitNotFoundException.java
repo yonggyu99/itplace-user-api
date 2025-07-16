d606fb5760c0e6c8c7db5dc60ea1fa8d32db46a7
hyunnk <jhyunnk30@gmail.com>
2025-07-16 15:36:28 +0900

    fix: 예외처리 변경

--- src/main/java/com/itplace/userapi/benefit/exception/BenefitNotFoundException.java
+++ src/main/java/com/itplace/userapi/benefit/exception/BenefitNotFoundException.java
@@ -1,10 +1,11 @@
 package com.itplace.userapi.benefit.exception;
 
 import com.itplace.userapi.common.BaseCode;
+import com.itplace.userapi.common.exception.BusinessException;
 import lombok.Getter;
 
 @Getter
-public class BenefitNotFoundException extends RuntimeException {
+public class BenefitNotFoundException extends BusinessException {
     private final BaseCode code;
 
     public BenefitNotFoundException(BaseCode code) {
