9d7c0f0594c3883ed63729002ff2a72fe3bb4dcb
HSH-11 <lyk1517@naver.com>
2025-07-16 17:56:06 +0900

    feat/membershiphistory Entity

---
+++ src/main/java/com/itplace/userapi/user/entity/MembershipHistory.java
@@ -0,0 +1,41 @@
+package com.itplace.userapi.user.entity;
+
+import com.itplace.userapi.benefit.entity.Benefit;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "membershiphistory")
+public class MembershipHistory {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "membershipId")
+    private Membership membership;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "benefitId")
+    private Benefit benefit;
+
+    @Id
+    private LocalDateTime usedAt;
+
+    private Long discountAmount;
+}
---
+++ src/main/java/com/itplace/userapi/user/entity/MembershipHistoryId.java
@@ -0,0 +1,16 @@
+package com.itplace.userapi.user.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MembershipHistoryId implements Serializable {
+    private String membership;
+    private Long benefit;
+    private LocalDateTime usedAt;
+}
