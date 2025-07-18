dbe929e7c27956c72a0af5303231dc9d3ff588b3
Eddie Lee <devleehy@gmail.com>
2025-07-18 13:20:15 +0900

    feat: 자체 로그인 설정 추가

--- src/main/java/com/itplace/userapi/security/config/SecurityConfig.java
+++ src/main/java/com/itplace/userapi/security/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.itplace.userapi.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itplace.userapi.security.auth.local.filter.LoginFilter;
+import com.itplace.userapi.security.jwt.JWTFilter;
+import com.itplace.userapi.security.jwt.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,14 +17,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -31,6 +42,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated());
+
+        LoginFilter loginFilter = new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, redisTemplate, objectMapper);
+        loginFilter.setFilterProcessesUrl("/api/v1/login");
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정 : STATELESS
         http
