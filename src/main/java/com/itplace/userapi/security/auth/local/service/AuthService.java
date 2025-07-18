6c9fdb0443e5dc59cabc65dc014a48de2be1d63e
Eddie Lee <devleehy@gmail.com>
2025-07-18 11:14:20 +0900

    feat: 불필요 메서드 삭제

--- src/main/java/com/itplace/userapi/security/auth/local/service/AuthService.java
+++ src/main/java/com/itplace/userapi/security/auth/local/service/AuthService.java
@@ -1,29 +1,18 @@
 package com.itplace.userapi.security.auth.local.service;
 
-import com.itplace.userapi.security.auth.local.dto.request.LinkLocalToOAuthRequest;
-import com.itplace.userapi.security.auth.local.dto.request.LinkOAuthToLocalRequest;
 import com.itplace.userapi.security.auth.local.dto.request.LoginRequest;
 import com.itplace.userapi.security.auth.local.dto.request.SignUpRequest;
 import com.itplace.userapi.security.auth.local.dto.request.UplusDataRequest;
-import com.itplace.userapi.security.auth.local.dto.response.LoginWithTokenResponse;
 import com.itplace.userapi.security.auth.local.dto.response.UplusDataResponse;
-import com.itplace.userapi.security.auth.oauth.dto.CustomOAuth2User;
-import com.itplace.userapi.security.auth.oauth.dto.OAuth2LoginResult;
 import java.util.Optional;
 
 public interface AuthService {
 
-    LoginWithTokenResponse login(LoginRequest request);
+    void login(LoginRequest request);
 
     void logout(Long userId);
 
     void signUp(SignUpRequest request);
 
-    OAuth2LoginResult oAuth2Login(CustomOAuth2User customOAuth2User);
-
     Optional<UplusDataResponse> uplusData(UplusDataRequest request);
-
-    void linkLocalToOAuth(LinkLocalToOAuthRequest request);
-
-    void linkOAuthToLocal(LinkOAuthToLocalRequest request);
 }
