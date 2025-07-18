d7476fe46a86b43afb6164f75e8422b0b3734bd5
Eddie Lee <devleehy@gmail.com>
2025-07-18 13:16:25 +0900

    feat: CustomUserDetails 로직 수정

--- src/main/java/com/itplace/userapi/security/auth/local/dto/CustomUserDetails.java
+++ src/main/java/com/itplace/userapi/security/auth/local/dto/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.itplace.userapi.security.auth.local.dto;
 
 import com.itplace.userapi.user.entity.User;
+import java.util.ArrayList;
 import java.util.Collection;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,18 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getAuthorities();
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole().getKey();
+            }
+        });
+        return collection;
+    }
+
+    public Long getUserId() {
+        return user.getId();
     }
 
     @Override
@@ -26,5 +38,25 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public String getPassword() {
         return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
