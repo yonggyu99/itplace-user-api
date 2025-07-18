76b7c7726fa2311f3794145585a950a23eb677c4
Eddie Lee <devleehy@gmail.com>
2025-07-18 11:17:03 +0900

    feat: DTO 변경

--- src/main/java/com/itplace/userapi/security/auth/local/dto/request/UplusDataRequest.java
+++ src/main/java/com/itplace/userapi/security/auth/local/dto/request/UplusDataRequest.java
@@ -1,10 +1,18 @@
 package com.itplace.userapi.security.auth.local.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UplusDataRequest {
-    String registrationId;
+
+    @NotBlank(message = "이름은 필수 항목입니다.")
+    private String name;
+
+    @NotBlank(message = "전화번호는 필수 항목입니다.")
+    @Pattern(regexp = "^010[0-9]{8}$", message = "전화번호 형식이 올바르지 않습니다.")
+    private String phoneNumber;
 }
