2f337757242fca187de1028d713772a9021db8b5
rxgx424 <gkfudrd1234@gmail.com>
2025-07-21 13:51:23 +0900

    feat: 사용자 로그 저장

--- src/main/java/com/itplace/userapi/ItplaceUserApiApplication.java
+++ src/main/java/com/itplace/userapi/ItplaceUserApiApplication.java
@@ -2,8 +2,25 @@ package com.itplace.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(  // jpa 사용되는 패키지
+        basePackages = {
+                "com.itplace.userapi.benefit",
+                "com.itplace.userapi.common",
+                "com.itplace.userapi.favorite",
+                "com.itplace.userapi.history",
+                "com.itplace.userapi.map",
+                "com.itplace.userapi.partner",
+                "com.itplace.userapi.security",
+                "com.itplace.userapi.user",
+        }
+)
+@EnableMongoRepositories(  // mongo 사용되는 패키지
+        basePackages = "com.itplace.userapi.log"
+)
 public class ItplaceUserApiApplication {
 
     public static void main(String[] args) {
