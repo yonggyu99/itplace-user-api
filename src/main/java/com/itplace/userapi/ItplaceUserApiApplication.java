1e48c5b164b5a86d303a9dee39eeda55a9f1c97d
Eddie Lee <devleehy@gmail.com>
2025-07-21 13:25:46 +0900

    feat: 스웨거 상대 URL 적용

--- src/main/java/com/itplace/userapi/ItplaceUserApiApplication.java
+++ src/main/java/com/itplace/userapi/ItplaceUserApiApplication.java
@@ -1,9 +1,12 @@
 package com.itplace.userapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class ItplaceUserApiApplication {
 
     public static void main(String[] args) {
