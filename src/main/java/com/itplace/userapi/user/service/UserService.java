1e814c41336e4946350b7d31de23dbb330924772
rxgx424 <gkfudrd1234@gmail.com>
2025-07-21 15:35:14 +0900

    refactor: 사용자 정보 조회 grade, param 변경

--- src/main/java/com/itplace/userapi/user/service/UserService.java
+++ src/main/java/com/itplace/userapi/user/service/UserService.java
@@ -1,14 +1,14 @@
 package com.itplace.userapi.user.service;
 
 import com.itplace.userapi.security.verification.email.dto.EmailConfirmRequest;
-import com.itplace.userapi.user.dto.UserInfoDto;
 import com.itplace.userapi.user.dto.request.FindEmailConfirmRequest;
 import com.itplace.userapi.user.dto.request.ResetPasswordRequest;
 import com.itplace.userapi.user.dto.response.FindEmailResponse;
 import com.itplace.userapi.user.dto.response.FindPasswordConfirmResponse;
+import com.itplace.userapi.user.dto.response.UserInfoResponse;
 
 public interface UserService {
-    UserInfoDto getUserInfo(Long userId);
+    UserInfoResponse getUserInfo(Long userId);
 
     FindEmailResponse findEmailConfirm(FindEmailConfirmRequest request);
 
