2dad3f19af53361b305bee73c69e0501444c2317
rxgx424 <gkfudrd1234@gmail.com>
2025-07-21 13:11:43 +0900

    feat: 사용자 로그 저장

--- src/main/java/com/itplace/userapi/security/auth/local/filter/LoginFilter.java
+++ src/main/java/com/itplace/userapi/security/auth/local/filter/LoginFilter.java
@@ -128,7 +128,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         Cookie cookie = new Cookie(JWTConstants.CATEGORY_ACCESS, token);
         cookie.setHttpOnly(true);
         cookie.setSecure(true);          // HTTPS 환경이면 true
-//        cookie.setDomain("itplace.click");
+        cookie.setDomain("itplace.click");
         cookie.setPath("/");
         long sec = jwtUtil.getAccessTokenValidityInMS() / 1000;
         cookie.setMaxAge((int) sec);
@@ -139,7 +139,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         Cookie cookie = new Cookie(JWTConstants.CATEGORY_REFRESH, token);
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
-//        cookie.setDomain("itplace.click");
+        cookie.setDomain("itplace.click");
         cookie.setPath("/");
         long sec = jwtUtil.getRefreshTokenValidityInMS() / 1000;
         cookie.setMaxAge((int) sec);
