57b82d2ced5345478feef1c9aadfab6eee7e2ae7
Eddie Lee <devleehy@gmail.com>
2025-07-22 16:35:05 +0900

    feat: Access 토큰 재발급 구현

--- src/main/java/com/itplace/userapi/security/auth/local/service/AuthServiceImpl.java
+++ src/main/java/com/itplace/userapi/security/auth/local/service/AuthServiceImpl.java
@@ -1,20 +1,29 @@
 package com.itplace.userapi.security.auth.local.service;
 
+import static com.itplace.userapi.security.auth.local.filter.LoginFilter.REFRESH_TOKEN_PREFIX;
+
 import com.itplace.userapi.security.SecurityCode;
 import com.itplace.userapi.security.auth.local.dto.request.SignUpRequest;
 import com.itplace.userapi.security.auth.local.dto.request.UplusDataRequest;
 import com.itplace.userapi.security.auth.local.dto.response.UplusDataResponse;
 import com.itplace.userapi.security.exception.DuplicateEmailException;
 import com.itplace.userapi.security.exception.DuplicatePhoneNumberException;
+import com.itplace.userapi.security.exception.InvalidCredentialsException;
 import com.itplace.userapi.security.exception.PasswordMismatchException;
+import com.itplace.userapi.security.jwt.JWTConstants;
+import com.itplace.userapi.security.jwt.JWTUtil;
 import com.itplace.userapi.user.entity.Role;
 import com.itplace.userapi.user.entity.User;
 import com.itplace.userapi.user.repository.UplusDataRepository;
 import com.itplace.userapi.user.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +38,68 @@ public class AuthServiceImpl implements AuthService {
     private final UplusDataRepository uplusDataRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void reissue(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Access 토큰 재발급 시작");
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(JWTConstants.CATEGORY_REFRESH)) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        String newAccessToken = validateRefreshTokenAndGetNewAccessToken(refreshToken);
+
+        // 새 Access Token을 쿠키에 담아 응답
+        ResponseCookie accessTokenCookie = ResponseCookie.from(JWTConstants.CATEGORY_ACCESS, newAccessToken)
+                .path("/")
+                .secure(true) // 로컬이 아닐때만 secure
+                .sameSite("None")
+                .httpOnly(true)
+                .maxAge(jwtUtil.getAccessTokenValidityInMS() / 1000)
+                .build();
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+    }
+
+    private String validateRefreshTokenAndGetNewAccessToken(String refreshToken) {
+        // Refresh Token 부재 시 예외 발생
+        if (refreshToken == null) {
+            log.info("리프레시 토큰이 없습니다.");
+            throw new InvalidCredentialsException(SecurityCode.REFRESH_TOKEN_REQUIRE);
+        }
+
+        // Refresh Token 만료 여부 확인
+        if (jwtUtil.isExpired(refreshToken)) {
+            log.info("리프레시 토큰이 만료되었습니다.");
+            throw new InvalidCredentialsException(SecurityCode.REFRESH_TOKEN_EXPIRED);
+        }
+
+        // 토큰 카테고리 확인
+        String category = jwtUtil.getCategory(refreshToken);
+        if (!category.equals(JWTConstants.CATEGORY_REFRESH)) {
+            log.info("잘못된 토큰 카테고리 입니다.");
+            throw new InvalidCredentialsException(SecurityCode.INVALID_TOKEN_TYPE);
+        }
+
+        // Redis에 저장된 토큰과 일치하는지 확인
+        Long userId = jwtUtil.getUserId(refreshToken);
+        String savedToken = redisTemplate.opsForValue().get(REFRESH_TOKEN_PREFIX + userId);
+        if (savedToken == null || !savedToken.equals(refreshToken)) {
+            log.info("사용자 정보와 일치하지 않는 토큰입니다.");
+            throw new InvalidCredentialsException(SecurityCode.INVALID_TOKEN);
+        }
+
+        // 새 Access Token 생성
+        String role = jwtUtil.getRole(refreshToken).getKey();
+        String newAccessToken = jwtUtil.createJwt(userId, role, JWTConstants.CATEGORY_ACCESS);
+        return newAccessToken;
+    }
 
     @Override
     public void logout(Long userId) {
