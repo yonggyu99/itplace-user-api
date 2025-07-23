37ffbd02a98cabc3cc72f070e9e1c2d0f215f9c3
Eddie Lee <devleehy@gmail.com>
2025-07-23 09:30:59 +0900

    feat: 멤버십 찾기 구현

--- src/main/java/com/itplace/userapi/user/repository/MembershipRepository.java
+++ src/main/java/com/itplace/userapi/user/repository/MembershipRepository.java
@@ -1,7 +1,9 @@
 package com.itplace.userapi.user.repository;
 
 import com.itplace.userapi.user.entity.Membership;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, String> {
+    Optional<Membership> findByMembershipId(String membershipId);
 }
