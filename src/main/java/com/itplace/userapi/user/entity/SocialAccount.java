75b6b1a2ad9f1243ba609c7b0d749e3d1ed6e5fe
Eddie Lee <devleehy@gmail.com>
2025-07-23 09:32:15 +0900

    feat: OAuth Entity 구현

---
+++ src/main/java/com/itplace/userapi/user/entity/SocialAccount.java
@@ -0,0 +1,42 @@
+package com.itplace.userapi.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "socialAccount", uniqueConstraints = {@UniqueConstraint(columnNames = {"provider", "providerId"})})
+public class SocialAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 20)
+    private String provider; // "kakao", "google" 등
+
+    @Column(nullable = false)
+    private String providerId; // 소셜 서비스의 고유 사용자 ID
+}
