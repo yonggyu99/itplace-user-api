f0156ee1cd4bb6a8c8607c8b7ad75742bd84ba8d
Eddie Lee <devleehy@gmail.com>
2025-07-23 09:32:52 +0900

    feat: SocialAccountRepository 구현

---
+++ src/main/java/com/itplace/userapi/user/repository/SocialAccountRepository.java
@@ -0,0 +1,9 @@
+package com.itplace.userapi.user.repository;
+
+import com.itplace.userapi.user.entity.SocialAccount;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+    Optional<SocialAccount> findByProviderAndProviderId(String provider, String providerId);
+}
