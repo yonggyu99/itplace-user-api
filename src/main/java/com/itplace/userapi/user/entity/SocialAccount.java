5db2f341ec9b2c3bb99b87498e51a8e6089a2d29
Eddie Lee <devleehy@gmail.com>
2025-07-23 15:23:08 +0900

    feat: BaseTimeEntity 상속

--- src/main/java/com/itplace/userapi/user/entity/SocialAccount.java
+++ src/main/java/com/itplace/userapi/user/entity/SocialAccount.java
@@ -1,5 +1,6 @@
 package com.itplace.userapi.user.entity;
 
+import com.itplace.userapi.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -24,7 +25,7 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(name = "socialAccount", uniqueConstraints = {@UniqueConstraint(columnNames = {"provider", "providerId"})})
-public class SocialAccount {
+public class SocialAccount extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
