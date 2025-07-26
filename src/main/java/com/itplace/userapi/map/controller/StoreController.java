eab39d432b304c447904e00a34d4e7fb055c7afe
rxgx424 <gkfudrd1234@gmail.com>
2025-07-27 02:01:57 +0900

    feat: 지도 검색 반환 방법 변경

--- src/main/java/com/itplace/userapi/map/controller/StoreController.java
+++ src/main/java/com/itplace/userapi/map/controller/StoreController.java
@@ -4,14 +4,13 @@ import com.itplace.userapi.common.ApiResponse;
 import com.itplace.userapi.map.StoreCode;
 import com.itplace.userapi.map.dto.StoreDetailDto;
 import com.itplace.userapi.map.service.StoreService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/maps")
@@ -25,7 +24,7 @@ public class StoreController {
             @RequestParam("lat") double lat,
             @RequestParam("lng") double lng,
             @RequestParam("radiusMeters") double radiusMeters
-    ){
+    ) {
         List<StoreDetailDto> stores = storeService.findNearby(lat, lng, radiusMeters);
         ApiResponse<?> body = ApiResponse.of(StoreCode.STORE_LIST_SUCCESS, stores);
 
@@ -39,8 +38,8 @@ public class StoreController {
             @RequestParam("lng") double lng,
             @RequestParam("radiusMeters") double radiusMeters,
             @RequestParam(value = "category", required = false) String category
-    ){
-        List<StoreDetailDto> stores = storeService.findNearbyByCategory(lat,lng,radiusMeters,category);
+    ) {
+        List<StoreDetailDto> stores = storeService.findNearbyByCategory(lat, lng, radiusMeters, category);
         ApiResponse<?> body = ApiResponse.of(StoreCode.STORE_LIST_SUCCESS, stores);
 
         return ResponseEntity.status(body.getStatus()).body(body);
@@ -51,11 +50,10 @@ public class StoreController {
     public ResponseEntity<ApiResponse<?>> getNearbySearch(
             @RequestParam("lat") double lat,
             @RequestParam("lng") double lng,
-            @RequestParam("radiusMeters") double radiusMeters,
             @RequestParam(value = "category", required = false) String category,
             @RequestParam("keyword") String keyword
-    ){
-        List<StoreDetailDto> stores = storeService.findNearbyByKeyword(lat,lng,radiusMeters,category,keyword);
+    ) {
+        List<StoreDetailDto> stores = storeService.findNearbyByKeyword(lat, lng, category, keyword);
         ApiResponse<?> body = ApiResponse.of(StoreCode.STORE_LIST_SUCCESS, stores);
 
         return ResponseEntity.status(body.getStatus()).body(body);
