61b3ca0c5e0e1ca94081069aca686d9b1565651d
Eddie Lee <devleehy@gmail.com>
2025-07-28 09:18:40 +0900

    feat: Uplus 데이터 연동 구현

--- src/main/java/com/itplace/userapi/user/service/UserService.java
+++ src/main/java/com/itplace/userapi/user/service/UserService.java
@@ -5,9 +5,11 @@ import com.itplace.userapi.security.verification.email.dto.EmailConfirmRequest;
 import com.itplace.userapi.user.dto.request.ChangePasswordRequest;
 import com.itplace.userapi.user.dto.request.FindEmailConfirmRequest;
 import com.itplace.userapi.user.dto.request.ResetPasswordRequest;
+import com.itplace.userapi.user.dto.response.CheckUplusDataResponse;
 import com.itplace.userapi.user.dto.response.FindEmailResponse;
 import com.itplace.userapi.user.dto.response.FindPasswordConfirmResponse;
 import com.itplace.userapi.user.dto.response.UserInfoResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 public interface UserService {
     UserInfoResponse getUserInfo(Long userId);
@@ -21,4 +23,8 @@ public interface UserService {
     void changePassword(PrincipalDetails principalDetails, ChangePasswordRequest request);
 
     void withdraw(Long userId, String password);
+
+    CheckUplusDataResponse checkUplusData(@AuthenticationPrincipal PrincipalDetails principalDetails);
+
+    void linkUplusData(@AuthenticationPrincipal PrincipalDetails principalDetails);
 }
